13bd4d0a181f7b8a47f909d406cd28ff1b95ec68
User Name <gugart@hotmail.com>
2020-09-03 19:30:44 -0300

    elements render Details

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -7,12 +7,14 @@ import {
   Dimensions,
   ScrollView,
   Image,
+  Alert,
 } from 'react-native';
 
 import { getCars } from '../../services/Cars';
 
 import { urlPhotos } from '../../services/Cars';
 import Colors from '../../styles/Colors/';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 let screenWidth = Dimensions.get('window').width;
 let screenHeight = Dimensions.get('window').height;
@@ -20,6 +22,11 @@ let screenHeight = Dimensions.get('window').height;
 const Details = ({ route, navigation }) => {
   const { idCar } = route.params;
   const [car, setCar] = useState(null);
+  const [confort, setConfort] = useState([]);
+  const [security, setSecurity] = useState([]);
+  const [multimedia, setMultimedia] = useState([]);
+  const [external, setExternal] = useState([]);
+  const [others, setOthers] = useState([]);
   const [element, setElement] = useState(null);
 
   useEffect(() => {
@@ -32,216 +39,187 @@ const Details = ({ route, navigation }) => {
     }
 
     async function loadCar() {
-      let confort = [];
-      let security = [];
-      let multimedia = [];
-      let external = [];
-      let others = [];
-
       //Confort
       if (car.ads.ar_condicionado == '1') {
-        confort.push('Ar Condicionado');
+        setConfort((confort) => [...confort, 'Ar Condicionado']);
       }
       if (car.ads.regulagem_altura == '1') {
-        confort.push('Banco com Regulagem de Altura');
+        setConfort((confort) => [...confort, 'Banco com Regulagem de Altura']);
       }
       if (car.ads.bancos_eletricos == '1') {
-        confort.push('Bancos Elétricos');
+        setConfort((confort) => [...confort, 'Bancos Elétricos']);
       }
       if (car.ads.bancos_couro == '1') {
-        confort.push('Bancos em Couro');
+        setConfort((confort) => [...confort, 'Bancos em Couro']);
       }
       if (car.ads.computador_bordo == '1') {
-        confort.push('Computador de Bordo');
+        setConfort((confort) => [...confort, 'Computador de Bordo']);
       }
       if (car.ads.retrovisores_eletricos == '1') {
-        confort.push('Controle de Retrovisores Elétricos');
+        setConfort((confort) => [
+          ...confort,
+          'Controle de Retrovisores Elétricos',
+        ]);
       }
       if (car.ads.direcao_hidraulica == '1') {
-        confort.push('Direção Hidráulica');
+        setConfort((confort) => [...confort, 'Direção Hidráulica']);
       }
       if (car.ads.encosto_traseiro == '1') {
-        confort.push('Encosto Traseiro');
+        setConfort((confort) => [...confort, 'Encosto Traseiro']);
       }
       if (car.ads.farois_automatica == '1') {
-        confort.push('Faróis com Regulagem Automática');
+        setConfort((confort) => [
+          ...confort,
+          'Faróis com Regulagem Automática',
+        ]);
       }
       if (car.ads.gps == '1') {
-        confort.push('GPS');
+        setConfort((confort) => [...confort, 'GPS']);
       }
       if (car.ads.piloto_automatico == '1') {
-        confort.push('Piloto Automático');
+        setConfort((confort) => [...confort, 'Piloto Automático']);
       }
       if (car.ads.volante_regulagem == '1') {
-        confort.push('Volante com Regulagem de Altura');
+        setConfort((confort) => [
+          ...confort,
+          'Volante com Regulagem de Altura',
+        ]);
       }
 
       //Security
       if (car.ads.airbag_motorista == '1') {
-        security.push('Airbag do Motorista');
+        setSecurity((security) => [...security, 'Airbag do Motorista']);
       }
       if (car.ads.airbag_passageiro == '1') {
-        security.push('Airbag do Passageiro');
+        setSecurity((security) => [...security, 'Airbag do Passageiro']);
       }
       if (car.ads.alarme == '1') {
-        security.push('Alarme');
+        setSecurity((security) => [...security, 'Alarme']);
       }
       if (car.ads.blindado == '1') {
-        security.push('Blindado');
+        setSecurity((security) => [...security, 'Blindado']);
       }
       if (car.ads.desembacador_traseiro == '1') {
-        security.push('Desembaçador Traseiro');
+        setSecurity((security) => [...security, 'Desembaçador Traseiro']);
       }
       if (car.ads.encosto_cabeca_traseiro == '1') {
-        security.push('Encosto de Cabeça Traseiro');
+        setSecurity((security) => [...security, 'Encosto de Cabeça Traseiro']);
       }
       if (car.ads.neblina_dianteiros == '1') {
-        security.push('Faróis de Neblina Dianteiros');
+        setSecurity((security) => [
+          ...security,
+          'Faróis de Neblina Dianteiros',
+        ]);
       }
       if (car.ads.neblina_traseiros == '1') {
-        security.push('Faróis de Neblina Traseiros');
-      }
-      if (car.ads.xenon == '1') {
-        security.push('Faróis Xenon');
+        setSecurity((security) => [...security, 'Faróis de Neblina Traseiros']);
       }
       if (car.ads.fechamento_automatico_vidros == '1') {
-        security.push('Fechamento Automático dos Vidros');
+        setSecurity((security) => [
+          ...security,
+          'Fechamento Automático dos Vidros',
+        ]);
       }
       if (car.ads.freios_abs == '1') {
-        security.push('Freios ABS');
-      }
-      if (car.ads.liga_leve == '1') {
-        security.push('Rodas de Liga Leve');
+        setSecurity((security) => [...security, 'Freios ABS']);
       }
       if (car.ads.sensor_chuva == '1') {
-        security.push('Sensor de Chuva');
+        setSecurity((security) => [...security, 'Sensor de Chuva']);
       }
       if (car.ads.sensor_estacionamento == '1') {
-        security.push('Sensor de Estacionamento');
+        setSecurity((security) => [...security, 'Sensor de Estacionamento']);
       }
       if (car.ads.sensor_luz == '1') {
-        security.push('Sensor de Luz');
+        setSecurity((security) => [...security, 'Sensor de Luz']);
       }
       if (car.ads.terceira_luz == '1') {
-        security.push('Terceira Luz de Freio LED');
+        setSecurity((security) => [...security, 'Terceira Luz de Freio LED']);
       }
       if (car.ads.teto_solar == '1') {
-        security.push('Teto Solar');
+        setSecurity((security) => [...security, 'Teto Solar']);
       }
       if (car.ads.trava_eletrica == '1') {
-        security.push('Travas Elétricas');
+        setSecurity((security) => [...security, 'Travas Elétricas']);
       }
       if (car.ads.vidros_eletricos == '1') {
-        security.push('Vidros Elétricos');
+        setSecurity((security) => [...security, 'Vidros Elétricos']);
       }
 
       //Multimedia
       if (car.ads.am_fm == '1') {
-        multimedia.push('AM/FM');
+        setMultimedia((multimedia) => [...multimedia, 'AM/FM']);
       }
       if (car.ads.bluetooth == '1') {
-        multimedia.push('Bluetooth');
+        setMultimedia((multimedia) => [...multimedia, 'Bluetooth']);
       }
       if (car.ads.cd_player == '1') {
-        multimedia.push('CD Player');
+        setMultimedia((multimedia) => [...multimedia, 'CD Player']);
       }
       if (car.ads.controle_remoto_volante == '1') {
-        multimedia.push('Controle Remoto no Volante');
+        setMultimedia((multimedia) => [
+          ...multimedia,
+          'Controle Remoto no Volante',
+        ]);
       }
       if (car.ads.dvd_player == '1') {
-        multimedia.push('DVD Player');
+        setMultimedia((multimedia) => [...multimedia, 'DVD Player']);
       }
       if (car.ads.entrada_auxiliar == '1') {
-        multimedia.push('Entrada Auxiliar');
+        setMultimedia((multimedia) => [...multimedia, 'Entrada Auxiliar']);
       }
       if (car.ads.entrada_usb == '1') {
-        multimedia.push('Entrada USB');
+        setMultimedia((multimedia) => [...multimedia, 'Entrada USB']);
       }
       if (car.ads.leitor_mp3 == '1') {
-        multimedia.push('Som com Leitor MP3');
+        setMultimedia((multimedia) => [...multimedia, 'Som com Leitor MP3']);
       }
 
       //External
       if (car.ads.capota == '1') {
-        external.push('Capota Marítima');
+        setExternal((extErnal) => [...extErnal, 'Capota Marítima']);
+      }
+      if (car.ads.xenon == '1') {
+        setExternal((extErnal) => [...extErnal, 'Faróis Xenon']);
       }
       if (car.ads.limpador_traseiro == '1') {
-        external.push('Limpador Traseiro');
+        setExternal((extErnal) => [...extErnal, 'Limpador Traseiro']);
       }
       if (car.ads.para_choques == '1') {
-        external.push('Pára-Choque na Cor do Veículo');
+        setExternal((extErnal) => [
+          ...extErnal,
+          'Pára-Choque na Cor do Veículo',
+        ]);
       }
       if (car.ads.protetor_cacamba == '1') {
-        external.push('Protetor de Caçamba');
+        setExternal((extErnal) => [...extErnal, 'Protetor de Caçamba']);
+      }
+      if (car.ads.liga_leve == '1') {
+        setExternal((extErnal) => [...extErnal, 'Rodas de Liga Leve']);
       }
 
       //Others
       if (car.ads.aceita_troca == '1') {
-        others.push('Aceita Troca');
+        setOthers((others) => [...others, 'Aceita Troca']);
       }
       if (car.ads.ipva_pago == '1') {
-        others.push('IPVA Pago');
+        setOthers((others) => [...others, 'IPVA Pago']);
       }
       if (car.ads.laudo_cautelar == '1') {
-        others.push('Laudo Cautelar');
+        setOthers((others) => [...others, 'Laudo Cautelar']);
       }
       if (car.ads.preco_negociavel == '1') {
-        others.push('Preço Negociável');
+        setOthers((others) => [...others, 'Preço Negociável']);
       }
       if (car.ads.unico_dono == '1') {
-        others.push('Único Dono');
+        setOthers((others) => [...others, 'Único Dono']);
       }
       if (car.ads.consignado == '1') {
-        others.push('Veículo Consignado');
+        setOthers((others) => [...others, 'Veículo Consignado']);
       }
+    }
 
-      //photoSlider
-      const photos = car.ads?.photo.length !== null ? car.ads.photo : null;
-
-      let images = [];
-
-      if (photos !== null) {
-        photos.map(({ idpic, thumbnail }) => {
-          let image_url = { uri: urlPhotos + thumbnail.replace(/thumb_/g, '') };
-          console.log('item');
-          console.log(image_url);
-          images.push(
-            <Image
-              key={idpic}
-              source={image_url}
-              resizeMethod="resize"
-              resizeMode="contain"
-              style={styles.image}
-            />
-          );
-        });
-      } else {
-        images.push(
-          <Image
-            source={require('../../assets/no_photo.png')}
-            resizeMethod="resize"
-            resizeMode="contain"
-            style={styles.image}
-          />
-        );
-      }
-
-      const PhotosElement = () => {
-        return (
-          <View>
-            <ScrollView
-              style={styles.slider}
-              horizontal={true}
-              pagingEnabled={true}
-            >
-              {images}
-            </ScrollView>
-            <Text>{`${car.ads.marca} ${car.ads.modelo} ${car.ads.versao}`}</Text>
-          </View>
-        );
-      };
-
-      setElement(<PhotosElement />);
+    async function loadElements() {
+      setElement(Elements);
     }
 
     if (car === null) {
@@ -249,12 +227,58 @@ const Details = ({ route, navigation }) => {
     }
     if (car !== null) {
       loadCar();
+      loadElements();
     }
   }, [car]);
 
+  const PhotosItem = () => {
+    const photos = car.ads?.photo.length !== null ? car.ads.photo : null;
+    return photos.map(({ idpic, thumbnail }) => {
+      return (
+        <Image
+          key={idpic}
+          source={{
+            uri: urlPhotos + thumbnail.replace(/thumb_/g, ''),
+          }}
+          resizeMethod="resize"
+          resizeMode="contain"
+          style={styles.image}
+        />
+      );
+    });
+  };
+
+  const onPressPhoto = () => {
+    Alert.alert('clicado');
+  };
+
+  const Elements = () => {
+    //Could be: const variable = <Element />
+    return (
+      <View>
+        <TouchableOpacity onPress={onPressPhoto()}>
+          <ScrollView
+            style={styles.slider}
+            horizontal={true}
+            pagingEnabled={true}
+          >
+            <PhotosItem />
+          </ScrollView>
+        </TouchableOpacity>
+        <Text>{`${car.ads.marca} ${car.ads.modelo} ${car.ads.versao}`}</Text>
+      </View>
+    );
+  };
+
   return (
     <View style={element == null ? styles.containerEmpty : styles.container}>
-      {element == null ? <ActivityIndicator size="large" /> : element}
+      {car == null ? (
+        <ActivityIndicator size="large" />
+      ) : element == null ? (
+        <ActivityIndicator size="large" />
+      ) : (
+        element
+      )}
     </View>
   );
 };
