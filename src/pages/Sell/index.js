c309e4b3da4931a0b6cec7fc8ff9851e994e449b
User Name <gugart@hotmail.com>
2020-10-02 17:31:36 -0300

    Sell

--- src/pages/Sell/index.js
+++ src/pages/Sell/index.js
@@ -1,14 +1,95 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import Logo from '../../components/logo';
+import SelectModal from '../../components/modalSelect';
+
+import { getFIPE } from '../../services/Cars';
+
+import Colors from '../../styles/Colors';
 
 const Sell = () => {
+  const [brandData, setBrandData] = useState(null);
+  const [modelData, setModelData] = useState(null);
+  const [brand, setBrand] = useState(null);
+  const [model, setModel] = useState(null);
+
+  useEffect(() => {
+    async function loadBrand() {
+      const data = await getFIPE({ urlComplement: 'marcas.json' });
+      console.log(data);
+      if (data.length > 0) {
+        setBrandData(data);
+      }
+    }
+
+    async function loadModel(id) {
+      let url = `veiculos/${id}.json`;
+      const data = await getFIPE({ urlComplement: url });
+      if (data.length > 0) {
+        setModelData(data);
+      }
+    }
+
+    if (brandData === null) {
+      loadBrand();
+    }
+    if (brand !== null) {
+      loadModel(brand.id);
+    }
+  }, []);
+
+  const onPressBrand = (e) => {
+    console.log(e);
+    setBrand(e);
+  };
+
+  const onPressModel = (e) => {
+    console.log(e);
+    setModel(e);
+  };
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Logo />
+      <ScrollView>
+        <Text style={styles.title}>
+          Deseja vender/trocar seu veículo?{'\n'}Preencha os dados abaixo!
+        </Text>
+        <SelectModal
+          title="Marca"
+          data={brandData}
+          placeHolder={brand === null ? 'Selecione a Marca' : brand.info}
+          onPress={onPressBrand}
+        />
+        {brand === null ? null : (
+          <SelectModal
+            title="Modelo"
+            data={modelData}
+            placeHolder={model === null ? 'Selecione o Modelo' : model.info}
+            onPress={onPressModel}
+          />
+        )}
+      </ScrollView>
     </View>
   );
 };
 
-export default Sell;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    paddingTop: 20,
+  },
+  title: {
+    textAlign: 'center',
+    marginTop: 15,
+    marginBottom: 10,
+    color: Colors.text,
+    fontSize: 20,
+    fontWeight: '300',
+  },
+});
 
-const styles = StyleSheet.create({});
+export default Sell;
--- src/services/Cars.js
+++ src/services/Cars.js
@@ -1,12 +1,40 @@
 import axios from 'axios';
 
-const url = 'https://www.germanoautomoveis.com.br/api/cars/';
+const urlGA = 'https://www.germanoautomoveis.com.br/api/cars/';
+const urlFIPE = 'http://fipeapi.appspot.com/api/1/carros/';
 
 export const getCars = async (data) => {
   const response = await axios
-    .post(url, data, { timeout: 60000 })
+    .post(urlGA, data, { timeout: 60000 })
     .then(function (response) {
       return response.data;
+    })
+    .catch(function (error) {
+      if (error.toString().toLowerCase().includes('timeout ')) {
+        return {
+          status: 'error',
+          message: 'Requisição ultrapassou o limite de tempo de resposta.',
+        };
+      } else {
+        return { status: 'error', message: error.message };
+      }
+    });
+
+  return response;
+};
+
+export const getFIPE = async (data) => {
+  let url = urlFIPE + data.urlComplement;
+  console.log(url);
+  const response = await axios
+    .get(url, { timeout: 60000 })
+    .then(function (response) {
+      let data = [];
+      response.data.map((item) => {
+        data.push({ id: item.id, info: item.name });
+      });
+      console.log(data);
+      return data;
     })
     .catch(function (error) {
       if (error.toString().toLowerCase().includes('timeout ')) {
